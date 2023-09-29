b7a7a89e5f5a95efce722a1e686ddbe767dd7f52
Hermann Rasch <hermann.rasch93@gmail.com>
2023-09-29 21:07:10 +0200

    Addition of the Put and Delete routes.

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-let dotenv = require("dotenv");
-dotenv.config();
+// let dotenv = require("dotenv");
+// dotenv.config();
+import "dotenv/config";
 import { Book } from "./models/bookModel.js";
 
 const app = express();
@@ -20,6 +21,7 @@ app.post("/books", async (request, response) => {
     try {
         if (
             !request.body.title ||
+            !request.body.subgenre ||
             !request.body.author ||
             !request.body.yearReleased
         ) {
@@ -70,8 +72,53 @@ app.get("books/:id", async (request, response) => {
         response.status(500).send({ message: error.message })
     }
 }) 
+// Route to update a single Book
+app.put("books/:id", async (request, response) => {
+    try{
+        if(
+            !request.body.title ||
+            !request.body.subgenre ||
+            !request.body.author ||
+            !request.body.yearReleased
+        ) {
+            return response.status(400).send({
+                message: "Send all required fields: title, author, yearReleased"
+            });
+        }
+        
+        const { id } = request.params;
+        
+        const result = await Book.findByIdAndUpdate(id, request.body); 
 
-mongoose.connect(mongoDBURL)
+        if (!result) {
+            return response.status(404).json({ message: "Book not found "});
+        }
+        return response.status(200).send({ message: " Book updated successfully"})
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+})
+// Route to delete a sinlge Book 
+app.delete("/books/:id", async (request, response) => {
+    try{
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndDelete(id);
+        
+        if (!result) {
+            return response.status(404).send({ message:"Book not found"});   
+        }
+
+        return response.status(200).send({ message:"Book deleted successfully"});
+        
+    }catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message})
+    }
+})
+
+mongoose.connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log("App connected to DB")
     app.listen(PORT, () => {
