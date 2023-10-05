9dabc3b50f430c8c939ed5fde99f5786667149e3
Hermann Rasch <hermann.rasch93@gmail.com>
2023-10-05 15:45:31 +0200

    Addition of routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../src/pages/Home";
+import EditBook from "../src/pages/EditBook"
+import CreateBook from "../src/pages/CreateBook"
+import DeleteBook from "../src/pages/DeleteBook"
+import ShowBook from "../src/pages/ShowBook"
 
 const App = () => {
   return (
-    <div className="bg-red-400 text-white" >App</div>
+    <Routes>
+      <Route path="/" element={<Home />} /> 
+      <Route path="/Books/edit/:id" element={<EditBook />} />
+      <Route path="/Books/create" element={<CreateBook />} />
+      <Route path="/Books/delete/:id" element={<DeleteBook />} />
+      <Route path="/Books/details/:id" element={<ShowBook />} />
+    </Routes>
   )
 }
 
