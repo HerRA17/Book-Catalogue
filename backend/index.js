1c087687c101703122fb61c1c32923880a2247b8
Hermann Rasch <hermann.rasch93@gmail.com>
2023-10-05 13:09:22 +0200

    CORS policy implemented

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,21 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js"
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request body
     app.use(express.json());
+
+// Middleware for handling CORS Policy
+app.use(
+    cors({
+        origin: "http://localhost:5555",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content Type"],
+    })
+) 
 
 app.get("/", (request, response) => {
     console.log(request)
