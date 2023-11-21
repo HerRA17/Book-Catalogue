f7b689739175983cfca9ffb0274494eb3b40b7e9
Hermann Rasch <hermann.rasch93@gmail.com>
2023-11-21 18:49:15 +0100

    Enabled CORS

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,14 @@ const app = express();
     app.use(express.json());
 
 // Middleware for handling CORS Policy
-app.use(
-    cors({
-        origin: "http://localhost:5555, http://127.0.0.1:5173/",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content Type"],
-    })
-) 
+app.use(cors());
+// app.use(
+//     cors({
+//         origin: "http://localhost:5555, http://127.0.0.1:5173/",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         allowedHeaders: ["Content Type"],
+//     })
+// ) 
 
 app.get("/", (request, response) => {
     console.log(request)
