d4912a1d2892348971022652815e3bb9000cabe1
Hermann Rasch <hermann.rasch93@gmail.com>
2023-11-27 19:26:01 +0100

    Small typo correction

--- Frontend/src/home/BookSingleCard.jsx
+++ Frontend/src/home/BookSingleCard.jsx
@@ -6,7 +6,7 @@ import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 
-const BookSingleCard = ({ books }) =>{
+const BookSingleCard = ({ book }) =>{
     return (
     <div key={book._id} className="border-2  border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl">
     <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
