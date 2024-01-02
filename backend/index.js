51e4a3b7e7df67723adea0b262b0576d37221680
Hermann Rasch <hermann.rasch93@gmail.com>
2024-01-02 18:50:54 +0100

    Addition of vercel.json file & implementation of CORS

--- backend/index.js
+++ backend/index.js
@@ -11,14 +11,14 @@ const app = express();
     app.use(express.json());
 
 // Middleware for handling CORS Policy
-app.use(cors());
-// app.use(
-//     cors({
-//         origin: "http://localhost:5555, http://127.0.0.1:5173/",
-//         methods: ["GET", "POST", "PUT", "DELETE"],
-//         allowedHeaders: ["Content Type"],
-//     })
-// ) 
+// app.use(cors());
+app.use(
+    cors({
+        origin: ["http://localhost:5555", "http://127.0.0.1:5173/", "https://book-catalogue-backend-pi.vercel.app/"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content Type"],
+    })
+) 
 
 app.get("/", (request, response) => {
     console.log(request)
@@ -31,7 +31,7 @@ mongoose.connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true }
 .then(() => {
     console.log("App connected to DB")
     app.listen(PORT, () => {
-        console.log(`App is listenning in port: ${PORT}`)
+        console.log(`App is listening in port: ${PORT}`)
     });
 })
 .catch((error) => {
